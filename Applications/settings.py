bb6ab09036626bc677274f6832ef97f2f1e2e0db
The Meta <alexmeta517@gmail.com>
2022-03-29 13:03:29 +0300

    Second Host Commit

--- Applications/settings.py
+++ Applications/settings.py
@@ -178,9 +178,3 @@ SOCIAL_AUTH_PIPELINE = (
 
 
 #The Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alexmeta517@gmail.com'
-EMAIL_HOST_PASSWORD = 'ceoMehta2030'
